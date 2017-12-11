775f62f80baee67e48f0d6c1a51c0721852b9a1a
asmaldone <alsmaldone@yahoo.com>
2017-12-11 18:53:18 -0500

    Spring property injection fake datasource example

--- spring/spring5/di-demo/src/main/java/com/avs/didemo/DiDemoApplication.java
+++ spring/spring5/di-demo/src/main/java/com/avs/didemo/DiDemoApplication.java
@@ -4,6 +4,7 @@ import com.avs.didemo.controllers.ConstructorInjectedController;
 import com.avs.didemo.controllers.MyController;
 import com.avs.didemo.controllers.PropertyInjectedController;
 import com.avs.didemo.controllers.SetterInjectedController;
+import com.avs.didemo.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -16,10 +17,13 @@ public class DiDemoApplication {
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
         MyController controller = (MyController) ctx.getBean("myController");
+
+        FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
+        System.out.println(fakeDataSource.getUser());
         //controller.hello();
-        System.out.println(controller.hello());
+      /*  System.out.println(controller.hello());
         System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
         System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
-        System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+        System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());*/
     }
 }
---
+++ spring/spring5/di-demo/src/main/java/com/avs/didemo/config/PropertyConfig.java
@@ -0,0 +1,34 @@
+package com.avs.didemo.config;
+
+import com.avs.didemo.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@PropertySource("classpath:datasource.properties")
+public class PropertyConfig {
+    @Value("${avs.username}")
+    String user;
+    @Value("${avs.password}")
+    String password;
+    @Value("${avs.url}")
+    String url;
+
+    @Bean
+    public FakeDataSource fakeDataSource() {
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUser(user);
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setUrl(url);
+        return fakeDataSource;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties() {
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+        return propertySourcesPlaceholderConfigurer;
+    }
+}
