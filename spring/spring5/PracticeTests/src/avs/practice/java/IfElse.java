93ddfe020432bc050c1bfe1be87dd2f7de528156
asmaldone <alsmaldone@yahoo.com>
2018-02-04 12:53:42 -0500

    Code practice

--- spring/spring5/PracticeTests/src/avs/practice/java/IfElse.java
+++ spring/spring5/PracticeTests/src/avs/practice/java/IfElse.java
@@ -6,11 +6,20 @@ public class IfElse {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int number=  scanner.nextInt();
+        int number = scanner.nextInt();
 
-        if(number%2==0) {
-            System.out.println("Not Weird");
-        }else {
+        if (number % 2 == 0) {
+
+
+            if (number >= 2 && number <= 5) {
+                System.out.println("Not Weird");
+            } else if (number >= 6 && number <= 20) {
+                System.out.println("Weird");
+            } else if (number > 20) {
+                System.out.println("Not Weird");
+            }
+
+        } else {
             System.out.println("Weird");
         }
     }
